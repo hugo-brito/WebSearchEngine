00ad5edbd83fe12e44e7cb60879c95e2b0a8a3ba
Hugo Brito <hubr@itu.dk>
2018-12-10 22:54:13 +0100

    changes to the aspect of the results

--- static/code.js
+++ static/code.js
@@ -1,6 +1,5 @@
 // JavaScript Document
 
-
 $(document).ready(function() {
     var baseUrl = "http://localhost:8080";
 
@@ -12,19 +11,36 @@ $(document).ready(function() {
             data: {query: $('#searchbox').val()}
         }).success( function (data) {
             console.log("Received response " + data);
-			
-            $("#responsesize").html("<p>Your busca for <strong>" + $('#searchbox').val() + "</strong> retrieved <strong>" + data.length + "</strong> results.</p>");
+
+            if (data.length === 0) {
+                $("#responseSize").html("<h4>Your busca for <strong>" + $('#searchbox').val() + "</strong> did not retrieve any results.</h4>");}
+            else {
+                $("#responseSize").html("<h4>Your busca for <strong>" + $('#searchbox').val() + "</strong> retrieved <strong>" + data.length + "</strong> results.</h4>");}
+
             var buffer = "";
-			
+
 //			if data.length>10
             $.each(data, function(index, value) {
-                buffer += "<p><a href=\"" + value.url + "\">" + value.title + "</a></p>";
+                buffer += "<div class='result'><a href=\'" + value.url + "'><h1>" + value.title + "</h1><h2>" + value.url + "</h2><h3>";
+                var element = 0; var limit = 15;
+                $.each(value.words, function (index, val) {
+                    if (element <= limit) {
+                        buffer += val + " ";
+                        element++;
+                    }
+
+                });
+                buffer += "...</h3></a></div>";
             });
-            $("#urllist").html(buffer);
+            $("#resultWrapper").html(buffer);
         });
     });
-	$(document).keypress(function(e) {
-    if(e.which == 13) {
+// 	$(document).keypress(function(e) {
+//     if(e.which == 13) {
+//         document.getElementById("searchbox").addEventListener("change", myFunction);
+//     function() {
+//         document.getElementById("searchbutton").click();
+//     }
 //        $("#searchbutton").click(function() {
 //        console.log("Sending request to server.");
 //        $.ajax({
@@ -33,10 +49,10 @@ $(document).ready(function() {
 //            data: {query: $('#searchbox').val()}
 //        }).success( function (data) {
 //            console.log("Received response " + data);
-//			
+//
 //            $("#responsesize").html("<p>Your busca for <strong>" + $('#searchbox').val() + "</strong> retrieved <strong>" + data.length + "</strong> results.</p>");
 //            var buffer = "";
-//			
+//
 ////			if data.length>10
 //            $.each(data, function(index, value) {
 //                buffer += "<p><a href=\"" + value.url + "\">" + value.title + "</a></p>";
@@ -44,8 +60,8 @@ $(document).ready(function() {
 //            $("#urllist").html(buffer);
 //        });
 //    });
-		alert('You pressed enter!');
-		
-    }
-});
+// 		alert('You pressed enter!');
+//
+//     }
+// });
 });

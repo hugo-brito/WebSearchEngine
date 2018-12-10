5b0762795e8dd806e47eac03b52d204fb73f4b5f
Hugo Brito <hubr@itu.dk>
2018-12-10 16:38:09 +0100

    accepting the enter key as trigger agent for the search

--- static/code.js
+++ static/code.js
@@ -23,4 +23,29 @@ $(document).ready(function() {
             $("#urllist").html(buffer);
         });
     });
+	$(document).keypress(function(e) {
+    if(e.which == 13) {
+//        $("#searchbutton").click(function() {
+//        console.log("Sending request to server.");
+//        $.ajax({
+//            method: "GET",
+//            url: baseUrl + "/search",
+//            data: {query: $('#searchbox').val()}
+//        }).success( function (data) {
+//            console.log("Received response " + data);
+//			
+//            $("#responsesize").html("<p>Your busca for <strong>" + $('#searchbox').val() + "</strong> retrieved <strong>" + data.length + "</strong> results.</p>");
+//            var buffer = "";
+//			
+////			if data.length>10
+//            $.each(data, function(index, value) {
+//                buffer += "<p><a href=\"" + value.url + "\">" + value.title + "</a></p>";
+//            });
+//            $("#urllist").html(buffer);
+//        });
+//    });
+		alert('You pressed enter!');
+		
+    }
+});
 });

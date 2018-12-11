363b8a639ae0bad50c21a2c62198158aef0d098c
Hugo Brito <hubr@itu.dk>
2018-12-11 11:42:05 +0100

    tiny improvement on the front end to account for empty queries

--- static/code.js
+++ static/code.js
@@ -12,27 +12,35 @@ $(document).ready(function() {
         }).success( function (data) {
             console.log("Received response " + data);
 
-            if (data.length === 0) {
-                $("#responseSize").html("<h4>Your busca for <strong>" + $('#searchbox').val() + "</strong> did not retrieve any results.</h4>");}
-            else {
-                $("#responseSize").html("<h4>Your busca for <strong>" + $('#searchbox').val() + "</strong> retrieved <strong>" + data.length + "</strong> results.</h4>");}
+            if ($("#searchbox").val() === "") { // empty query
+                $("#responseSize").html("<h4>Please type something to busca for.</h4>");}
 
-            var buffer = "";
+            else {
+
+                if (data.length === 0) {
+                    $("#responseSize").html("<h4>Your busca for <strong>" + $('#searchbox').val() + "</strong> did not retrieve any results.</h4>"); // no results
+                } else {
+                    $("#responseSize").html("<h4>Your busca for <strong>" + $('#searchbox').val() + "</strong> retrieved <strong>" + data.length + "</strong> results.</h4>");
+                }
+
+                var buffer = "";
 
 //			if data.length>10
-            $.each(data, function(index, value) {
-                buffer += "<div class='result'><a href=\'" + value.url + "'><h1>" + value.title + "</h1><h2>" + value.url + "</h2><h3>";
-                var element = 0; var limit = 15;
-                $.each(value.words, function (index, val) {
-                    if (element <= limit) {
-                        buffer += val + " ";
-                        element++;
-                    }
+                $.each(data, function (index, value) {
+                    buffer += "<div class='result'><a href=\'" + value.url + "'><h1>" + value.title + "</h1><h2>" + value.url + "</h2><h3>";
+                    var element = 0;
+                    var limit = 15;
+                    $.each(value.words, function (index, val) {
+                        if (element <= limit) {
+                            buffer += val + " ";
+                            element++;
+                        }
 
+                    });
+                    buffer += "...</h3></a></div>";
                 });
-                buffer += "...</h3></a></div>";
-            });
-            $("#resultWrapper").html(buffer);
+                $("#resultWrapper").html(buffer);
+            }
         });
     });
 // 	$(document).keypress(function(e) {

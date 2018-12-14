c4826979842646b8df4250453f08d95d151c560d
Jonas <haja@itu.dk>
2018-12-14 11:17:04 +0100

    Finished tests for OkapiBM25. Minor edit in RankScoreTest

--- src/test/java/searchengine/RankScoreTest.java
+++ src/test/java/searchengine/RankScoreTest.java
@@ -20,19 +20,19 @@ public class RankScoreTest {
         sites.add(new Website("1.com", "example1", Arrays.asList(words1)));
 
         // 2.com mentions 13 words: Queen= 2; Denmark= 2; of= 6; randomword = 2
-        String[] words2 = ("Queen of Denmark Queen of Denmark of of of of randomword  randomword ").toLowerCase().split(" ");
+        String[] words2 = ("Queen of Denmark Queen of Denmark of of of of randomword randomword ").toLowerCase().split(" ");
         sites.add(new Website("2.com", "example2", Arrays.asList(words2)));
 
         //3.com mentions 14 words: Queen= 1; Denmark= 1; of= 10; randomword = 2
-        String[] words3 = ("Queen of Denmark of of of of of of of of of randomword  randomword ").toLowerCase().split(" ");
+        String[] words3 = ("Queen of Denmark of of of of of of of of of randomword randomword ").toLowerCase().split(" ");
         sites.add(new Website("3.com", "example3", Arrays.asList(words3)));
 
         //4.com mentions 9 words: of= 7; randomword = 2 (Website adds noise for word "of")
-        String[] words4 = ("of of of of of of of randomword  randomword ").toLowerCase().split(" ");
+        String[] words4 = ("of of of of of of of randomword randomword ").toLowerCase().split(" ");
         sites.add(new Website("4.com", "example4", Arrays.asList(words4)));
 
         //5.com mentions 9 words: of= 7; randomword = 2 (Website adds noise for word "of")
-        String[] words5 = ("of of of of of of of randomword  randomword ").toLowerCase().split(" ");
+        String[] words5 = ("of of of of of of of randomword randomword ").toLowerCase().split(" ");
         sites.add(new Website("5.com", "example5", Arrays.asList(words5)));
 
 
@@ -61,5 +61,4 @@ public class RankScoreTest {
         assertEquals(sites.get(1),rankedMatchedSites.get(1));
         assertEquals(sites.get(2),rankedMatchedSites.get(2));
     }
-
 }
